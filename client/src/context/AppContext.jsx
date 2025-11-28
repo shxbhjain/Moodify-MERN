6235a503b2788d4cf3b244db2c3bf16b936e4fa1
Shubh Jain <shubhjain211@gmail.com>
2025-11-28 23:22:25 +0530

    Update AppContext.jsx

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -9,7 +9,7 @@ export const AppContext = createContext()
 export const AppContextProvider = ({ children }) => {
 
     axios.defaults.withCredentials=true;
-    const backendUrl = import.meta.env.VITE_BACKEND_URL || 'http://localhost:4000'
+    const backendUrl = import.meta.env.VITE_BACKEND_URL || 'https://moodify-mern.vercel.app/'
     const [isLoggedin, setIsLoggedin] = useState(false)
     const [userData, setUserData] = useState(false)
 
